14b636f2458b86ace33021397631a1e26672e9fb
jiaxiaokang <jiaxiaokang@famingo-inc.com>
2019-02-27 11:28:14 +0800

    add new info

---
+++ two_sum.go
@@ -0,0 +1,21 @@
+package main
+import (
+    "fmt"
+)
+func twoSum(nums[]int, target int) []int {
+    length := len(nums)
+    for i := 0; i <= length - 1; i++ {
+        for j := i; j <= length - 1; j++ {
+            if (nums[i] + nums[j] == target) {
+                return []int{i,j}
+            }
+        }
+    }
+    return []int{}
+}
+
+func main() {
+    nums := []int{3,2,4}
+    target := 6
+    fmt.Println(twoSum(nums, target))
+}
---
+++ two_sum2.go
@@ -0,0 +1,27 @@
+package main
+import (
+    "fmt"
+)
+func twoSum(nums[]int, target int) []int {
+    numMap := make(map[int]int)
+    for index, n := range nums {
+        numMap[n] = index
+    }
+    length := len(nums)
+    for i := 0; i <= length - 1; i++ {
+        head := target - nums[i] 
+        index, ok := numMap[head]
+        if ok  && index != i{
+            return []int{i, index}
+        }
+        
+
+    }
+    return []int{}
+}
+
+func main() {
+    nums := []int{3,2,4}
+    target := 6
+    fmt.Println(twoSum(nums, target))
+}
